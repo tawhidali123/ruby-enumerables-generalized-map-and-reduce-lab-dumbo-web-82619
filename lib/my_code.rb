ce1a6a2c7caca616240b9bc9313d18aebdd674e4
Tawhid Ali <tawhidali126@gmail.com>
2019-08-06 18:54:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+
+def map(source_array, &block)
+  source_array.map(&block)
+end
+
+def reduce(source_array, starting_point=0, &block)
+  if starting_point > 0 
+    source_array.reduce(starting_point, &block)
+  else 
+    source_array.reduce(&block)
+  end
+end
+
+
+
+
