249154996cf9d6f658b4252d413890e51d27fdbc
Tawhid Ali <tawhidali126@gmail.com>
2019-08-06 18:28:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
 # Your Code Here
+
+def map(source_array, &block)
+  source_array.map(&block)
+end
+
+def reduce(source_array, starting_point=0, &block)
+  source_array.reduce(starting_point, &block)
+end
+
+
+
+
