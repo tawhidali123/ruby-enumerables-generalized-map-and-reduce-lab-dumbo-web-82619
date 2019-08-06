acb9427e0c7a5238b84f676fc8c5084ffe6425e3
Tawhid Ali <tawhidali126@gmail.com>
2019-08-06 18:51:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,14 +4,14 @@ def map(source_array, &block)
   source_array.map(&block)
 end
 
-def reduce(source_array, starting_point=0, &block)
+def reduce(source_array, starting_point*, &block)
   # source_array.reduce(starting_point, &block)
-  if starting_point = true
-    # source_array.reduce(starting_point, &block)
-    source_array.reduce(&block)
-  else 
-    # source_array.reduce(&block)
+  if starting_point = true 
     source_array.reduce(starting_point, &block)
+    # source_array.reduce(&block)
+  else 
+    source_array.reduce(&block)
+    # source_array.reduce(starting_point, &block)
   end
 end
 
