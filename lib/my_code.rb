91941adcad3bcb432cff4eeda6b42674143dabcf
Tawhid Ali <tawhidali126@gmail.com>
2019-08-06 18:47:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,12 +6,12 @@ end
 
 def reduce(source_array, starting_point=0, &block)
   # source_array.reduce(starting_point, &block)
-  if starting_point = 0
-    # source_array.reduce(starting_point, &block)
-    source_array.reduce(&block)
-  else 
-    # source_array.reduce(&block)
+  if starting_point 
     source_array.reduce(starting_point, &block)
+    # source_array.reduce(&block)
+  else 
+    source_array.reduce(&block)
+    # source_array.reduce(starting_point, &block)
   end
 end
 
