b208ad112646c4babcf585eed1047e98430dc6ce
Tawhid Ali <tawhidali126@gmail.com>
2019-08-06 18:54:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map(source_array, &block)
   source_array.map(&block)
 end
 
-def reduce(source_array, starting_point, &block)
+def reduce(source_array, starting_point=0, &block)
   if starting_point > 0 
     source_array.reduce(starting_point, &block)
   else 
